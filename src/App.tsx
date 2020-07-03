89d6c95ad2c509e1d8fddda02777003159cd88ec
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-07-02 22:12:14 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
+
+type Todo = {
+  text: string;
+  complete: boolean;
+};
+
+const todos: Array<Todo> = [
+  { text: "dqwdwq", complete: false },
+  { text: "efe", complete: true },
+];
 
 function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+  return <TodoListItem/>;
 }
 
 export default App;
