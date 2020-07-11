3023604ee6188581895e2dd3701ddb87f852fd1d
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-07-10 23:02:23 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css';
+import { TodoList } from './TodoList';
+
+let c1 = (
+  <div className="center">
+    <App />
+    <div >yysfss</div>
+  </div>
+);
+
+let c2 = React.createElement('div', null, '!!!!', 'fwwefewfwfewfewfw');
+
+let c3 = React.createElement('div', { className: 'center' }, React.createElement('h1', null, 'fewfewfe'));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  c1,
   document.getElementById('root')
 );
+/* ReactDOM.render(
+  <div>dvfsefesfsefes</div>,
+  document.getElementById('a')
+);
+ */
+
 
 
